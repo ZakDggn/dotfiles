ebb4f4054b4e4640b66df73b3066d2cb665e09cd
ZakDggn <23458039+ZakDggn@users.noreply.github.com>
2023-04-02 01:39:01 +0100

    Added colorscheme to neovim config

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -14,6 +14,7 @@ local packer_bootstrap = ensure_packer()
 
 return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
+  use 'folke/tokyonight.nvim'
 
   if packer_bootstrap then
     require('packer').sync()
