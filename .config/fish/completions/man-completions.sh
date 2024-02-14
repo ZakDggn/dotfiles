e2271d50ff70e2e22890324f8f0936a9f8251c44
ZakDggn <23458039+ZakDggn@users.noreply.github.com>
2024-02-14 23:11:39 +0000

    Updated neovim config and gitignore, removed custom fish man completion

--- .config/fish/completions/man-completions.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-for i in 1 5 8; do
-    find /usr/share/man/man${i}/ -type f -printf '%f\n' | sed -E "s/\.${i}\.gz//"
-done
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,5 +6,6 @@ if status is-interactive
     alias ll='ls -lav'
     alias grep='grep --color=auto'
     alias config='git --git-dir=$HOME/.dotfiles --work-tree=$HOME'
+    alias lvim='NVIM_APPNAME=nvim-lazyvim nvim'
     starship init fish | source
 end
