50e9cc3eabe6cf0df20252142c7981abfb84a34f
ZakDggn <23458039+ZakDggn@users.noreply.github.com>
2024-11-02 22:58:53 +0000

    Breeze cursor, Roboto font for UI, Waybar keyd module

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,7 +6,6 @@ if status is-interactive
     alias ll='ls -lav'
     alias grep='grep --color=auto'
     alias config='git --git-dir=$HOME/.dotfiles --work-tree=$HOME'
-    alias lvim='NVIM_APPNAME=nvim-lazyvim nvim'
     alias conda-init='eval /opt/miniconda3/bin/conda "shell.fish" "hook" $argv | source && conda activate env'
     starship init fish | source
 end
--- .config/sway/autostart.sh
+++ .config/sway/autostart.sh
@@ -11,7 +11,6 @@ start gammastep-indicator -l 51:0 -t 6500:3000
 start nm-applet
 start earlyoom -r 3600 -n --avoid '(^|/)(init|systemd.*|Xorg|sshd)$'
 start systembus-notify
-#start ~/dwm/keyd-indicator
 start swayidle -w \
     timeout 300 'chayang -d 10 && swaymsg "output * dpms off"' \
         resume 'swaymsg "output * dpms on"' \
