d7c1048b885d957cd21895935a3884ad449560a4
ZakDggn <23458039+ZakDggn@users.noreply.github.com>
2024-11-11 14:45:38 +0000

    Update fish config for use as default shell, minor changes

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,15 +1,23 @@
+set -g fish_greeting
+if status is-login
+    if test -z "$WAYLAND_DISPLAY" -a "$XDG_VTNR" = 1
+        exec start-sway &> .sway.log
+    end
+end
 if status is-interactive
-    set -g fish_greeting
     alias cat='bat'
     alias ls='lsd'
     alias l='ls -lv'
     alias ll='ls -lav'
     alias grep='grep --color=auto'
     alias config='git --git-dir=$HOME/.dotfiles --work-tree=$HOME'
-    alias conda-init='eval /opt/miniconda3/bin/conda "shell.fish" "hook" $argv | source && conda activate env'
     if string match -eq kitty $TERM
         alias clear='printf "\e[H\e[3J"'
+        alias sshpi='kitten ssh pi@192.168.1.10'
     end
-    starship init fish | source
+    if test $TERM = linux
+        fish_config theme choose Old\ School
+    else
+        starship init fish | source
+    end
 end
-
